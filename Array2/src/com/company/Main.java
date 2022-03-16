0e57238df5e81507b43ae714909ae219952ff303
nycujan <aur4@mail.ru>
2022-03-16 18:47:58 +0300

    first commit

---
+++ Array2/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] gb = {8, 16, 32, 64, 128, 256, 512, 1024};
+        for (int i = 0; i < gb.length; i++) {
+            System.out.printf(gb[i] + " ");
+        }
+
+
+
+    }
+}
