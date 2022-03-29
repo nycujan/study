5f83f55c684b129eeb007e9586c7b8dc2344f2b9
nycujan <aur4@mail.ru>
2022-03-29 20:12:12 +0300

    gitignore

--- Array2/src/com/company/Main.java
+++ Array2/src/com/company/Main.java
@@ -1,15 +1,17 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        int[] gb = {8, 16, 32, 64, 128, 256, 512, 1024};
-        for (int i = 0; i < gb.length; i++) {
-            System.out.printf(gb[i] + " ");
+        ArrayList<Integer> gb = Arrays.asList (8, 16, 32, 64, 128, 256, 1024);
+        {
+            System.out.println(gb);
         }
 
-
-
     }
+
+
 }
